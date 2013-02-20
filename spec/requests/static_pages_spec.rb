74209b0516da71a2d0bc3fb588873a038f6b4a63
Josh Shatkin-Margolis <jshatkinmargolis@gmail.com>
2013-02-20 13:49:57 -0500

    Finish sign in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,8 +5,8 @@ describe "Static pages" do
   subject { page }
 
   shared_examples_for "all static pages" do
-    it { should have_selector('h1',    text: heading) }
-    it { should have_selector('title', text: full_title(page_title)) }
+    it { should have_header_message(heading) }
+    it { should have_title_message(full_title(page_title)) }
   end
 
   describe "Home page" do
@@ -45,15 +45,15 @@ describe "Static pages" do
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
-    page.should have_selector 'title', text: full_title('About Us')
+    page.should have_title_message full_title('About Us')
     click_link "Help"
-    page.should have_selector 'title', text: full_title('Help')
+    page.should have_title_message full_title('Help')
     click_link "Contact"
-    page.should have_selector 'title', text: full_title('Contact')
+    page.should have_title_message full_title('Contact')
     click_link "Home"
     click_link "Sign up now!"
-    page.should have_selector 'title', text: full_title('Sign up')
+    page.should have_title_message full_title('Sign up')
     click_link "sample app"
-    page.should have_selector 'title', text: full_title('')
+    page.should have_title_message full_title('')
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,15 +8,15 @@ describe "User pages" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }
+    it { should have_header_message(user.name) }
+    it { should have_title_message(user.name) }
   end
 
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_header_message('Sign up') }
+    it { should have_title_message(full_title('Sign up')) }
   end
 
   describe "signup" do
@@ -33,7 +33,7 @@ describe "User pages" do
       describe "after submission" do
         before { click_button submit }
 
-        it { should have_selector('title', text: 'Sign up') }
+        it { should have_title_message('Sign up') }
         it { should have_content('error') }
         it { should have_content('Name can\'t be blank') }
         it { should have_content('Email can\'t be blank') }
@@ -60,8 +60,9 @@ describe "User pages" do
         before { click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
 
-        it { should have_selector('title', text: user.name) }
+        it { should have_title_message(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
